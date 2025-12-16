23df152632687a0b9a9dd8b1b7112799d14b407c
Patrick Schlieker <patrick.schlieker@gmail.com>
2025-12-16 12:54:56 +0100

    error handling

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,30 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 3 {
-        panic!("minigrep needs two arguments: <search text> <path/to/file.txt>");
-    }
-
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1)
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
     println!("With text:\n\n{contents}");
+    Ok(())
 }
 
 struct Config {
@@ -24,9 +32,12 @@ struct Config {
     file_path: String,
 }
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("minigrep needs two arguments: <search text> <path/to/file.txt>");
+        }
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Config { query, file_path }
+        return Ok(Config { query, file_path });
     }
 }
