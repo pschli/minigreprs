0ab29ee3bbcd6762001a6af6d5f26530c66e4f4a
Patrick Schlieker <patrick.schlieker@gmail.com>
2025-12-16 10:41:15 +0100

    passing arguments

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,10 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+
+    if args.len() >= 3 {
+        println!("{}, {}", args[1], args[2]);
+    } else {
+        println!("minigrep needs two arguments: <searchstring> <path/to/file.txt>")
+    }
 }
