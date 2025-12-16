12f11476c63c976b2048c5059160f7deb8a6e59f
Patrick Schlieker <patrick.schlieker@gmail.com>
2025-12-16 12:40:25 +0100

    refactoring to structs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,32 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() >= 3 {
-        println!("{}, {}", args[1], args[2]);
-    } else {
-        println!("minigrep needs two arguments: <searchstring> <path/to/file.txt>")
+    if args.len() < 3 {
+        panic!("minigrep needs two arguments: <search text> <path/to/file.txt>");
+    }
+
+    let config = Config::new(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Config { query, file_path }
     }
 }
